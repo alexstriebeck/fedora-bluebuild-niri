83f0b58a2779cb9d589830737b1424e392e8b046
alexstriebeck <alex.striebeck+github@gmail.com>
2026-04-22 20:45:11 +0200

    update backrest.sh

--- files/scripts/backrest.sh
+++ files/scripts/backrest.sh
@@ -8,5 +8,5 @@ set -xoue pipefail
 cd /tmp
 mkdir backrest
 wget https://github.com/garethgeorge/backrest/releases/download/v1.12.1/backrest_Linux_x86_64.tar.gz
-tar tar -xzvf backrest_Linux_x86_64.tar.gz -C backrest
+tar -xzvf backrest_Linux_x86_64.tar.gz -C backrest
 sudo mv backrest /usr/local/bin/backrest
